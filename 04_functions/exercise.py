8209e5f2952b123069c67490e718e57d88d6440c
Brandon Ly <Bvl@usf.edu>
2023-10-13 18:55:12 -0400

    did the exercises and changed the names

--- 04_functions/exercise.py
+++ 04_functions/exercise.py
@@ -1,5 +1,8 @@
+# Complete the function by making sure it returns 42. .
 def return_42():
-    return 42
+    return(42)
+    pass  # 'pass' just means "do nothing". Make sure to delete this!
 
-def my_function(arg1, arg2):
-    return arg1 * arg2
+# Create a function below, called my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_function(x, y):
+    return x * y
